26de432d7572a9e8af28672bf621837c9898a41f
Robert Buecker <robert.buecker@rigaku.com>
2023-11-15 16:27:39 +0100

    new classes for managing finalizations

---
+++ cap_tools/finalization.py
@@ -0,0 +1,170 @@
+from collections.abc import MutableMapping
+import pandas as pd
+import os
+from io import StringIO
+from typing import Dict, Iterator, MutableMapping, Tuple
+
+try:
+    import gemmi
+    HAVE_GEMMI = True
+except ImportError:
+    HAVE_GEMMI = False   
+
+class Finalization:
+
+    HEADLINE = 'Statistics vs resolution (taking redundancy into account) - Laue group'
+
+    def __init__(self, path: str, verbose: bool = True):
+
+        self.basename = path
+        self.verbose = verbose
+
+        if HAVE_GEMMI and os.path.exists(path + '.mtz'):
+            self.mtz = gemmi.read_mtz_file(path + '.mtz')
+            if verbose:
+                print(f'Parsed reflection file {path + ".mtz"}')
+
+        sh, ov = self.parse_finalization_results()
+
+        if (sh is None) or (ov is None):
+            raise RuntimeError(f'Could not parse {path}')
+
+        self.shells = sh
+        self.overall = ov
+
+    def parse_finalization_results(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
+
+        fn = self.basename + '_red.sum'
+
+        table = ''
+
+        if not os.path.exists(fn):
+            print(f'Result summary file {fn} not found.')
+            return None, None
+
+        if self.verbose:
+            print(f'Parsing result summary file {fn}')
+
+        with open(fn, 'r') as fh:
+            # slice out result table from summary file (do not parse values yet)
+            parsing = False
+            for ln in fh:
+                # print(ln)
+                if parsing and not ln.strip():
+                    parsing = False
+                    continue
+                elif parsing:
+                    table += ln
+                elif ln.startswith(self.HEADLINE):
+                    table = ''
+                    parsing = True
+                    continue
+
+        if not table:
+            print(f'No result table found in {fn}')
+            return None, None
+        else:
+            if self.verbose:
+                print(f'Found result table in {fn}:')
+                print(table)
+
+        sh, res, overall = StringIO(table), StringIO(), StringIO()
+        _, cols, _ = [sh.readline() for _ in range(3)] # get header lines
+
+        # parse shell and overall data
+        parse_ov = False
+        for ln in sh:
+            if ln.startswith('-----------------------------'):
+                parse_ov = True
+                continue
+            if parse_ov:
+                overall.write(ln)
+            else:
+                res.write(ln)
+        res.seek(0), overall.seek(0)
+
+        # mangle column names
+        cols = cols.replace('tion(A)', 'dmax dmin').replace('CC 1/2', 'CC1/2').split()
+
+        # import final data into Pandas dataframes and mangle a bit
+        shells = pd.read_csv(res, skiprows=0, header=None, delim_whitespace=True, names=cols)
+        overall = pd.read_csv(overall, skiprows=0, header=None, delim_whitespace=True, names=cols)
+        shells['dmax'] = shells['dmax'].str.split('-',expand=True)[0]
+        overall['dmax'] = overall['dmax'].str.split('-',expand=True)[0]
+
+        return shells, overall
+
+
+class FinalizationCollection(MutableMapping[str, Finalization]):
+
+    @classmethod
+    def from_folder(cls, folder: str, include_subfolders: bool = False,
+                    ignore_parse_errors: bool = False, **kwargs):
+        from glob import glob
+        paths = [fn[:-8]
+                 for fn in glob(os.path.join(folder, '*_red.sum'))]
+
+        fc = cls()
+
+        for path in paths:
+            try:
+                fc[os.path.basename(path)] = Finalization(path, **kwargs)
+            except RuntimeError as err:
+                if ignore_parse_errors:
+                    print(f'{path} could not be parsed, skipping.')
+                else:
+                    raise err
+
+        return fc
+
+    def __init__(self):
+        super().__init__()
+        self._finalizations: Dict[str, Finalization] = {}
+
+    def __setitem__(self, key: str, value: Finalization):
+        self._finalizations[key] = value
+
+    def __getitem__(self, key) -> Finalization:
+        try:
+            return self._finalizations[key]
+        except KeyError as err:
+            raise KeyError(f'Finalization {key} not found in collection')
+
+    def __len__(self) -> int:
+        return len(self._finalizations)
+
+    def __delitem__(self, key: str):
+        del self._finalizations[key]
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(self._finalizations)
+
+    @property
+    def overall(self) -> pd.DataFrame:
+        summary = []
+        for k, v in self.items():
+            ov = v.overall.iloc[[0],:].copy()
+            ov['name'] = k
+            summary.append(ov)
+
+        return pd.concat(summary, join='outer')
+
+    @property
+    def shelldata(self) -> pd.DataFrame:
+        allshell = []
+        for k, v in self.items():
+            shells = v.shells.copy()
+            shells['name'] = k
+            allshell.append(shells)
+
+        return pd.concat(allshell, join='outer')
+
+    @property
+    def highest_shell(self) -> pd.DataFrame:
+        allshell = []
+        for k, v in self.items():
+            shells = v.shells.iloc[[-1],:].copy()
+            shells['name'] = k
+            allshell.append(shells)
+
+        return pd.concat(allshell, join='outer')
